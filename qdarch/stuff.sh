5bfc2c6bb1df4d9b263946524c7c44ad6a398d23
l3f7s1d3 <59766814+l3f7s1d3@users.noreply.github.com>
2020-01-11 23:41:08 +0100

    Update stuff.sh

--- qdarch/stuff.sh
+++ qdarch/stuff.sh
@@ -1,1 +1,10 @@
 
+#other stuff
+pacman -S cdrtools gedit screenfetch firefox openssh transmission-gtk --noconfirm
+
+#Virtualbox
+pacman -S virtualbox virtualbox-host-modules-arch --noconfirm
+gpasswd -a $USERNAME vboxusers
+
+#fastboot android
+#pacman -S fastboot --noconfirm
