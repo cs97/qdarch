f3d2ed18f35040438efd9e4107a6348cc33c9b02
l3f7s1d3 <59766814+l3f7s1d3@users.noreply.github.com>
2020-01-12 18:19:21 +0100

    Update doinchroot.sh

--- qdarch/doinchroot.sh
+++ qdarch/doinchroot.sh
@@ -51,7 +51,7 @@ pacman -S xorg-twm xorg-xclock xterm --noconfirm
 echo "exec i3" >> ~/.xinitrc
 
 #Desktop packete
-pacman -S i3 feh ttf-dejavu scrot thunar file-roller i3lock gvfs alsa alsa-utils pulseaudio sudo --noconfirm
+pacman -S i3 feh ttf-dejavu scrot thunar file-roller i3lock gvfs alsa alsa-utils pulseaudio sudo conky--noconfirm
 
 #wifi
 pacman -S wpa_supplicant netctl dialog --noconfirm
