0a0a3ee0f3c2734372bf6b1fcbfbb54572835974
l3f7s1d3 <59766814+l3f7s1d3@users.noreply.github.com>
2020-01-13 17:57:33 +0100

    Update doinchroot.sh

--- qdarch/doinchroot.sh
+++ qdarch/doinchroot.sh
@@ -88,5 +88,12 @@ passwd $USERNAME
 #wallpaper
 #cp /root/installer/tpbh.png /home/$USERNAME/tpbh.png
 
+#other stuff
+pacman -S cdrtools gedit screenfetch firefox openssh transmission-gtk --noconfirm
 
+#Virtualbox
+pacman -S virtualbox virtualbox-host-modules-arch --noconfirm
+gpasswd -a $USERNAME vboxusers
 
+#fastboot android
+#pacman -S fastboot --noconfirm
